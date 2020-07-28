bf1f37f4d1a6f69e382183a3ba27657f82566659
Vincent Seyller <vseyller@laposte.net>
2020-07-29 00:22:03 +0200

    Ajout graduation axes X Y

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function afficher() {
 function afficherAxes(contexte) {
     var width = contexte.canvas.width;
     var height = contexte.canvas.height;
+    var tmp;
 
     contexte.beginPath();
     
@@ -25,9 +26,23 @@ function afficherAxes(contexte) {
     contexte.moveTo(0, height/2);
     contexte.lineTo(width, height/2);
 
+    // Graduation axe X
+    for(tmp = 0; tmp < width; tmp += 10){
+        contexte.moveTo(tmp, height/2);
+        contexte.lineTo(tmp, height/2 + 5);
+        contexte.lineTo(tmp, height/2 - 5);
+    }
+
     // Création axe Y
     contexte.moveTo(width/2, 0);
     contexte.lineTo(width/2, height);
+
+    // Graduation axe Y
+    for(tmp = 0; tmp < height; tmp += 10){
+        contexte.moveTo(width/2, tmp);
+        contexte.lineTo(width/2 + 5, tmp);
+        contexte.lineTo(width/2 - 5, tmp);
+    }
     
     contexte.stroke();
 }
