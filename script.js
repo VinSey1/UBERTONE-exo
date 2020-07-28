5175048ddeff8b388c954b70a86cce1b52bca0fc
Vincent Seyller <vseyller@laposte.net>
2020-07-29 00:21:03 +0200

    Affichage axes X Y canvas

--- script.js
+++ script.js
@@ -10,4 +10,24 @@ function afficher() {
 
     document.getElementById("afficherAmp").innerHTML = amplitude;
     document.getElementById("afficherFreq").innerHTML = frequence;
+
+    afficherAxes(contexte);
+    contexte.save();
+}
+
+function afficherAxes(contexte) {
+    var width = contexte.canvas.width;
+    var height = contexte.canvas.height;
+
+    contexte.beginPath();
+    
+    // Création axe X
+    contexte.moveTo(0, height/2);
+    contexte.lineTo(width, height/2);
+
+    // Création axe Y
+    contexte.moveTo(width/2, 0);
+    contexte.lineTo(width/2, height);
+    
+    contexte.stroke();
 }
